2cce7e50deccd4359670d007beca34a455d154b3
maacecas <maacecas@gmail.com>
2026-09-16 15:14:31 -0500

    Auto-commit de cambios (Streaming DPC): mié 16/09/2026 15:14:31

---
+++ Iniciar_Bot_Local.bat
@@ -0,0 +1,6 @@
+@echo off
+title Streaming DPC - Bot Local de Validacion de TV
+echo Iniciando el Bot de Netflix TV Localmente...
+cd /d "f:\Antigravity\Antigravity\streaming-dpc"
+node local-bot.js
+pause
